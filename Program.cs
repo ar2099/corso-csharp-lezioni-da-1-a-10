18bf838cfe62cbb5a7309da7cf274025d7823ffd
ar2099 <onemanshow2099@gmail.com>
2022-09-15 16:31:17 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace corso_csharp_lezioni_da_1_a_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
